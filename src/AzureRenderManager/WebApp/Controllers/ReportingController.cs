87c63b1c4988bfc96841ad107a27036fa8b1d8bf
George Pollard <porges@porg.es>
2019-02-19 13:39:06 +1300

    Reörganize usage code
    This will make it easier to access from other controllers.

--- src/AzureRenderManager/WebApp/Controllers/ReportingController.cs
+++ src/AzureRenderManager/WebApp/Controllers/ReportingController.cs
@@ -1,18 +1,14 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Caching.Memory;
-using TaskTupleAwaiter;
 using WebApp.Code.Attributes;
 using WebApp.Code.Contract;
-using WebApp.Config;
 using WebApp.CostManagement;
 using WebApp.Models.Reporting;
-using static System.FormattableString;
 
 namespace WebApp.Controllers
 {
@@ -22,30 +18,27 @@ namespace WebApp.Controllers
     {
         private static readonly TimeSpan CacheResultsFor = TimeSpan.FromMinutes(10);
 
-        private readonly CostManagementClientAccessor _clientAccessor;
-        private readonly IMemoryCache _memoryCache;
+        private readonly IUsageCoordinator _usageCoordinator;
 
         public ReportingController(
             IEnvironmentCoordinator environmentCoordinator,
             IPackageCoordinator packageCoordinator,
             IAssetRepoCoordinator assetRepoCoordinator,
-            CostManagementClientAccessor clientAccessor,
-            IMemoryCache memoryCache)
+            IUsageCoordinator usageCoordinator)
             : base(environmentCoordinator, packageCoordinator, assetRepoCoordinator)
         {
-            _clientAccessor = clientAccessor;
-            _memoryCache = memoryCache;
+            _usageCoordinator = usageCoordinator;
         }
 
         [HttpGet]
         [Route("Reporting", Name = nameof(Index))]
         public async Task<ActionResult> Index([FromQuery] DateTimeOffset? from, [FromQuery] DateTimeOffset? to)
         {
-            var (envs, client) = await (Environments(), _clientAccessor.GetClient());
+            var envs = await Environments();
 
             var period = GetQueryPeriod(from, to);
 
-            var usages = await Task.WhenAll(envs.Select(env => GetUsage(client, env, period)));
+            var usages = await Task.WhenAll(envs.Select(env => _usageCoordinator.GetUsage(env, period)));
 
             var nextMonthLink = GetNextMonthLink(period);
             var currentMonthLink = GetCurrentMonthLink();
@@ -65,11 +58,11 @@ namespace WebApp.Controllers
         [Route("Reporting/{envId}", Name = nameof(Environment))]
         public async Task<ActionResult<EnvironmentUsage>> Environment(string envId, [FromQuery] DateTimeOffset? from, [FromQuery] DateTimeOffset? to)
         {
-            var (env, client) = await (Environment(envId), _clientAccessor.GetClient());
+            var env = await Environment(envId);
 
             var period = GetQueryPeriod(from, to);
 
-            var usage = await GetUsage(client, env, period);
+            var usage = await _usageCoordinator.GetUsage(env, period);
 
             var nextMonthLink = GetNextMonthLink(period);
             var currentMonthLink = GetCurrentMonthLink();
@@ -148,47 +141,6 @@ namespace WebApp.Controllers
         {
             var today = DateTimeOffset.UtcNow;
             return new QueryTimePeriod(from: StartOfMonth(today), to: EndOfMonth(today));
-        }
-
-        private async Task<EnvironmentUsage> GetUsage(
-            CostManagementClient client,
-            RenderingEnvironment env,
-            QueryTimePeriod timePeriod)
-        {
-            var cacheKey = Invariant($"REPORTING:{env.Name}/{timePeriod.From}/{timePeriod.To}");
-
-            var usageResponse = await _memoryCache.GetOrCreateAsync(cacheKey, Fetch); 
-
-            if (usageResponse.Properties == null)
-            {
-                return new EnvironmentUsage(env.Name, null);
-            }
-            
-            return new EnvironmentUsage(env.Name, new Usage(timePeriod, usageResponse));
-
-            Task<UsageResponse> Fetch(ICacheEntry ice)
-            {
-                ice.AbsoluteExpirationRelativeToNow = CacheResultsFor;
-                
-                var usageRequest =
-                    new UsageRequest(
-                        Timeframe.Custom,
-                        new Dataset(
-                            Granularity.Daily,
-                            new Dictionary<string, Aggregation>
-                            {
-                            { "totalCost", new Aggregation(AggregationFunction.Sum, "PreTaxCost") }
-                            },
-                            new List<Grouping>
-                            {
-                            new Grouping("MeterSubCategory", ColumnType.Dimension)
-                            },
-                            FilterExpression.Tag("environment", Operator.In, new[] { env.Id })));
-
-                usageRequest.TimePeriod = timePeriod;
-
-                return client.GetUsageForSubscription(env.SubscriptionId, usageRequest);
-            }
         }
     }
 }
