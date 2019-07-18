ae24fc8331e2cdfb1476e135afbcd53ba5c7fada
Christian Smith <christis@microsoft.com>
2019-07-19 09:02:01 +1000

    Fix signout

--- src/AzureRenderManager/WebApp/Controllers/AccountController.cs
+++ src/AzureRenderManager/WebApp/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.AzureAD.UI;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Mvc;
